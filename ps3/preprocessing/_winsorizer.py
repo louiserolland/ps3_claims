a2076aede82c0d2e781afd221dd66f42dbf441c9
louiserolland <louiserolland04@gmail.com>
2025-11-23 22:43:49 +0000

    added winsorizer and tested it - everything passed

--- ps3/preprocessing/_winsorizer.py
+++ ps3/preprocessing/_winsorizer.py
@@ -1,15 +1,31 @@
+#%%
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
 
 # TODO: Write a simple Winsorizer transformer which takes a lower and upper quantile and cuts the
 # data accordingly
+
+
+#%%
 class Winsorizer(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        pass
+    def __init__(self, lower_quantile, upper_quantile):
+        self.lower_quantile = lower_quantile
+        self.upper_quantile = upper_quantile
 
     def fit(self, X, y=None):
-        pass
+        X = np.asarray(X)
+        self.lower_ = np.quantile(X, self.lower_quantile, axis=0)
+        self.upper_ = np.quantile(X, self.upper_quantile, axis=0)
+        return self
 
     def transform(self, X):
-        pass
+        check_is_fitted(self, ["lower_", "upper_"])
+        X = np.asarray(X)
+        X_winsorized = np.clip(X, self.lower_, self.upper_)
+        return X_winsorized
+    
+
+
+
+# %%
--- tests/preprocessing/test_winsorizer.py
+++ tests/preprocessing/test_winsorizer.py
@@ -11,4 +11,24 @@ def test_winsorizer(lower_quantile, upper_quantile):
 
     X = np.random.normal(0, 1, 1000)
 
-    assert False
+    # Reshape to 2D because your transformer expects 2D arrays
+    X = X.reshape(-1, 1)
+
+    # Initialize your Winsorizer
+    w = Winsorizer(
+        lower_quantile=lower_quantile,
+        upper_quantile=upper_quantile
+    )
+
+    # Fit and transform
+    w.fit(X)
+    X_wins = w.transform(X)
+
+    # Compute expected result using numpy
+    lower = np.quantile(X, lower_quantile, axis=0)
+    upper = np.quantile(X, upper_quantile, axis=0)
+    expected = np.clip(X, lower, upper)
+
+    # The transformed result must match numpy's clipping
+    assert np.allclose(X_wins, expected)
+
